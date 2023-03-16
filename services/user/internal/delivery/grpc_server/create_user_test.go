6f12bf9f3b6f04eb9f393404690be45bd7dc3c8f
hadekha <hadekha.fitra@salt.co.id>
2023-03-16 13:10:31 +0700

    add unit test create user

---
+++ services/user/internal/delivery/grpc_server/create_user_test.go
@@ -0,0 +1,123 @@
+package grpcserver
+
+import (
+	"context"
+	"errors"
+	userpb "grpcrest/proto/_generated/user"
+	"grpcrest/services/user/domain/usecase"
+	"grpcrest/services/user/internal/config"
+	"grpcrest/services/user/mocks"
+	"grpcrest/services/user/test/testdata"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"google.golang.org/grpc/peer"
+)
+
+func TestCreateUser(t *testing.T) {
+	type field struct {
+		userUC usecase.UserUsecase
+		cfg    *config.AppConfig
+	}
+
+	type args struct {
+		ctx  context.Context
+		user *userpb.CreateUserRequest
+	}
+
+	mockUserUCSuccess := new(mocks.UserUsecase)
+	mockUserUCSuccess.On("Create", mock.AnythingOfType("*entity.User")).Return(nil)
+	mockUserUCFail := new(mocks.UserUsecase)
+	mockUserUCFail.On("Create", mock.AnythingOfType("*entity.User")).Return(errors.New("some error"))
+	p := &peer.Peer{Addr: &net.TCPAddr{IP: net.IPv4(1, 2, 3, 4), Port: 8888}}
+	doubleCfg := &config.AppConfig{}
+
+	testcases := []struct {
+		Name  string
+		Args  args
+		Field field
+		Err   error
+	}{
+		{
+			Name: "success",
+			Field: field{
+				userUC: mockUserUCSuccess,
+				cfg:    doubleCfg,
+			},
+			Args: args{
+				ctx:  peer.NewContext(context.TODO(), p),
+				user: testdata.NewCreateUserRequestFull(),
+			},
+			Err: nil,
+		},
+		{
+			Name: "missing name",
+			Field: field{
+				userUC: mockUserUCSuccess,
+				cfg:    doubleCfg,
+			},
+			Args: args{
+				ctx:  peer.NewContext(context.TODO(), p),
+				user: testdata.NewCreateUserRequestNoName(),
+			},
+			Err: errors.New("some error"),
+		},
+		{
+			Name: "missing email",
+			Field: field{
+				userUC: mockUserUCSuccess,
+				cfg:    doubleCfg,
+			},
+			Args: args{
+				ctx:  peer.NewContext(context.TODO(), p),
+				user: testdata.NewCreateUserRequestNoEmail(),
+			},
+			Err: errors.New("some error"),
+		},
+		{
+			Name: "missing address",
+			Field: field{
+				userUC: mockUserUCSuccess,
+				cfg:    doubleCfg,
+			},
+			Args: args{
+				ctx:  peer.NewContext(context.TODO(), p),
+				user: testdata.NewCreateUserRequestNoAddress(),
+			},
+			Err: errors.New("some error"),
+		},
+		{
+			Name: "missing password",
+			Field: field{
+				userUC: mockUserUCSuccess,
+				cfg:    doubleCfg,
+			},
+			Args: args{
+				ctx:  peer.NewContext(context.TODO(), p),
+				user: testdata.NewCreateUserRequestNoPassword(),
+			},
+			Err: errors.New("some error"),
+		},
+	}
+
+	for _, v := range testcases {
+		t.Run(v.Name, func(t *testing.T) {
+			//initiate delivery
+			s := server{
+				userUC: v.Field.userUC,
+				cfg:    v.Field.cfg,
+			}
+
+			//execute
+			_, err := s.CreateUser(v.Args.ctx, v.Args.user)
+			if v.Err == nil {
+				assert.Nil(t, err)
+			} else {
+				assert.NotNil(t, err)
+			}
+
+		})
+	}
+}
--- services/user/test/testdata/user.go
+++ services/user/test/testdata/user.go
@@ -1,6 +1,7 @@
 package testdata
 
 import (
+	userpb "grpcrest/proto/_generated/user"
 	"grpcrest/services/user/domain/entity"
 
 	"github.com/bxcodec/faker/v4"
@@ -14,4 +15,37 @@ func NewUserEntity() *entity.User {
 		Address:  faker.Word(),
 		Password: faker.Password(),
 	}
+}
+
+func NewCreateUserRequestFull() *userpb.CreateUserRequest {
+	return &userpb.CreateUserRequest{
+		Name:     faker.Name(),
+		Email:    faker.Email(),
+		Address:  faker.Word(),
+		Password: faker.Password(),
+	}
+}
+
+func NewCreateUserRequestNoName() *userpb.CreateUserRequest {
+	v := NewCreateUserRequestFull()
+	v.Name = ""
+	return v
+}
+
+func NewCreateUserRequestNoEmail() *userpb.CreateUserRequest {
+	v := NewCreateUserRequestFull()
+	v.Email = ""
+	return v
+}
+
+func NewCreateUserRequestNoAddress() *userpb.CreateUserRequest {
+	v := NewCreateUserRequestFull()
+	v.Address = ""
+	return v
+}
+
+func NewCreateUserRequestNoPassword() *userpb.CreateUserRequest {
+	v := NewCreateUserRequestFull()
+	v.Password = ""
+	return v
 }
